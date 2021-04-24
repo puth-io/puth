80a4ff057770a138116e05e84424147cb792e329
Sebastian Ühlin <uehlinse@gmail.com>
2021-04-24 02:58:02 +0200

    Added retry to gui and open prompt if connection failed

--- gui/src/App/WebsocketHandler.ts
+++ gui/src/App/WebsocketHandler.ts
@@ -2,11 +2,11 @@ import { action, makeAutoObservable, runInAction } from 'mobx';
 import { ICommand } from './Command';
 
 export type IContext = {
-  id: string,
-  commands: ICommand[],
-  logs: any[],
-  created: number,
-}
+  id: string;
+  commands: ICommand[];
+  logs: any[];
+  created: number;
+};
 
 class WebsocketHandlerSingleton {
   private websocket: WebSocket | undefined;
@@ -19,24 +19,52 @@ class WebsocketHandlerSingleton {
     makeAutoObservable(this);
   }
 
-  connect(uri?: string) {
+  try(uri: string) {
+    this.connect(uri, {
+      onclose: (event) => {
+        let wsUri = prompt('Websocket URI', uri);
+        // use setTimeout to clear callstack
+        setTimeout(() => this.try(wsUri));
+      },
+    });
+  }
+
+  connect(
+    uri: string = 'ws://127.0.0.1:4000/websocket',
+    options: {
+      retry?: boolean;
+      onclose?: (event: CloseEvent) => void;
+      timeout?: number;
+    } = {},
+  ) {
     this.uri = uri;
-    this.websocket = new WebSocket('ws://127.0.0.1:4000/websocket');
+    this.websocket = new WebSocket(this.uri);
+
+    const timeoutTimer = setTimeout(() => {
+      this.websocket.close();
+    }, options?.timeout ?? 3 * 1000);
 
     this.websocket.onopen = (event) => {
-      runInAction(() => this.connected = true);
+      clearTimeout(timeoutTimer);
+      runInAction(() => (this.connected = true));
     };
 
     this.websocket.onclose = (event) => {
-      runInAction(() => this.connected = false);
-      setTimeout(() => this.connect(this.uri), 1000);
+      runInAction(() => (this.connected = false));
+
+      if (options.retry) {
+        setTimeout(() => this.connect(this.uri), 1000);
+      }
+      if (options.onclose) {
+        options.onclose(event);
+      }
     };
 
     this.websocket.onmessage = (event) => {
       let data = JSON.parse(event.data);
 
       if (Array.isArray(data)) {
-        data.forEach(p => this.receivedPacket(p));
+        data.forEach((p) => this.receivedPacket(p));
       } else {
         this.receivedPacket(data);
       }
@@ -66,7 +94,7 @@ class WebsocketHandlerSingleton {
         commands: [],
         logs: [],
         created: Date.now(),
-      })
+      });
     }
 
     return this.contexts.get(id);
--- gui/src/index.tsx
+++ gui/src/index.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
-import {WebsocketHandler} from './App/WebsocketHandler';
+import { WebsocketHandler } from './App/WebsocketHandler';
 import mitt from 'mitt';
 
 export const Events = mitt();
 
 Events.on('*', (type, event) => {
-    console.log('[EventLogger]', type, event);
+  // tslint:disable-next-line:no-console
+  console.log('[EventLogger]', type, event);
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
-WebsocketHandler.connect();
+// Try to connect to websocket on same hostname like gui.
+WebsocketHandler.try('ws://' + window.location.host + '/websocket');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
