d2420c6bd67d60f1605c3d323eaa7933a4cf1126
Sebastian Ühlin <uehlinse@gmail.com>
2021-05-01 08:24:26 +0200

    Added development mode host auto connect to gui

--- gui/src/index.tsx
+++ gui/src/index.tsx
@@ -18,8 +18,15 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+let websocketFirstTryHost = 'ws://' + window.location.host + '/websocket';
+
+// Gets removed in production code by webpack
+if (process.env.NODE_ENV === 'development') {
+  websocketFirstTryHost = 'ws://127.0.0.1:4000/websocket';
+}
+
 // Try to connect to websocket on same hostname like gui.
-WebsocketHandler.try('ws://' + window.location.host + '/websocket');
+WebsocketHandler.try(websocketFirstTryHost);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
