0c8b891df793f8e9e7352922055cfa3abf8de9f5
Sebastian Uehlin <s.uehlin@proton.me>
2023-02-19 15:17:28 +0100

    wip

---
+++ bin/php-split.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+CURRENT_BRANCH="master"
+
+function split()
+{
+    SHA1=`./bin/splitsh-lite --prefix=$1`
+    git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
+}
+
+function remote()
+{
+    git remote add $1 $2 || true
+}
+
+git pull origin $CURRENT_BRANCH
+
+#remote php git@github.com:SEUH/puth-laravel.git
+remote laravel git@github.com:SEUH/puth-laravel.git
+
+#split 'workspaces/clients/php/php' php
+split 'workspaces/clients/php/laravel' laravel
