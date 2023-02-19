c4ec0cc114d3369521cb6d6514a5d3981e9951e1
Sebastian Uehlin <s.uehlin@proton.me>
2023-02-19 21:03:04 +0100

    wip

--- bin/tag-release.sh
+++ bin/tag-release.sh
@@ -20,7 +20,7 @@ do
     echo "Releasing puth $REMOTE";
 
     TMP_DIR="/tmp/puth-split"
-    REMOTE_URL="https://github.com/puth-io/$REMOTE.git"
+    REMOTE_URL="https://$GITHUB_TOKEN@github.com/puth-io/$REMOTE.git"
 
     rm -rf $TMP_DIR;
     mkdir $TMP_DIR;
