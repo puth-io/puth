384cf88f8e0de238b8c7f61fcca8e09115ad2b3b
Sebastian Uehlin <s.uehlin@proton.me>
2023-10-01 10:47:06 +0200

    cleanup

--- workspaces/clients/php/laravel/src/TestCase.php
+++ workspaces/clients/php/laravel/src/TestCase.php
@@ -23,13 +23,6 @@ abstract class TestCase extends FoundationTestCase
     
     private bool $isPhpVersion10;
     
-    public function __construct(string $name)
-    {
-        parent::__construct($name);
-        
-        $this->isPhpVersion10 = intval(explode('.', Version::id())[0]) > 9;
-    }
-    
     protected function setUp(): void
     {
         parent::setUp();
--- workspaces/clients/php/workspaces/laravel-10/config/puth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'instance' => [
-        'url' => env('PUTH_INSTANCE_URL', 'http://127.0.0.1:7345'),
-    ],
-    
-    'debug' => env('PUTH_DEBUG', false),
-];
