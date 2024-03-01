ce0f1942bddd62cf03a03c33dba5182ff2d168a5
Sebastian Uehlin <s.uehlin@proton.me>
2024-03-01 21:20:13 +0100

    test new ci/cd

---
+++ bin/builds-clear.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf workspaces/clients/js/client/lib \
+       workspaces/clients/js/client/tsconfig.tsbuildinfo \
+       workspaces/core/lib \
+       workspaces/core/tsconfig.tsbuildinfo \
+       workspaces/gui/dist \
+       workspaces/gui/tsconfig.tsbuildinfo \
+       workspaces/puth/lib \
+       workspaces/puth/tsconfig.tsbuildinfo
